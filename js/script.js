b16df490bd3e99fdd5ab00bae402004f5c5b52e4
Nedaa Marwan <nedaagamaal4321@gmail.com>
2025-11-12 18:35:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+let currentInput = "";
+
+function press(value) {
+    if (value === "Clear") {
+        currentInput = "";
+        display.value = "";
+    } else if (value === "=") {
+        try {
+            let expression = currentInput.replace(/X/g, "*").replace(/÷/g, "/").replace(/_/g, "-");
+            let result = eval(expression);
+            display.value = result;
+            currentInput = result.toString();
+        } catch {
+            display.value = "Error";
+            currentInput = "";
+        }
+    } else {
+        if (value === "X") value = "*";
+        if (value === "÷") value = "/";
+        if (value === "_") value = "-";
+        currentInput += value;
+        display.value = currentInput;
+    }
+}
